2ec583889fbf889ef61df68668d24a2aac4b7f80
Stephen Parsons <zup_dudes@yahoo.com>
2018-07-26 14:50:07 -0400

    Update Movie Required constraints on Model Update-Database Populate Genre Data

---
+++ VidlyUA/Migrations/201807261839518_Update-Movie-Column-Requirements.cs
@@ -0,0 +1,18 @@
+namespace VidlyUA.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateMovieColumnRequirements : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Movies", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Movies", "Name", c => c.String());
+        }
+    }
+}
--- VidlyUA/Models/Movie.cs
+++ VidlyUA/Models/Movie.cs
@@ -9,6 +9,9 @@ namespace VidlyUA.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         [Required]
