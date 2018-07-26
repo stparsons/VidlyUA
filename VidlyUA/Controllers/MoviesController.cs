849b2eaf114aeb10fbaf0e58600e18aedb9c8a42
Stephen Parsons <zup_dudes@yahoo.com>
2018-07-26 15:02:12 -0400

    Update Movie Controler to add ApplicationDbContext

--- VidlyUA/Controllers/MoviesController.cs
+++ VidlyUA/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,16 +10,28 @@ namespace VidlyUA.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
 
             return View(movies);    
         }
 
         public ActionResult Details( int id )
         {
-            var movie = GetMovies().SingleOrDefault( c => c.Id == id );
+            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault( c => c.Id == id );
 
             if ( movie == null )
                 return HttpNotFound();
@@ -26,14 +39,5 @@ namespace VidlyUA.Controllers
             return View( movie );
         }
 
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
-        }
- 
     }
 }
