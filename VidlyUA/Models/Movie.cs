76010c4390d4271c86eaf64436ec1304833eba52
Stephen Parsons <zup_dudes@yahoo.com>
2018-07-26 14:16:06 -0400

    Update Movies - Link to Genre and add properties. Update Database Migration

--- VidlyUA/Models/Movie.cs
+++ VidlyUA/Models/Movie.cs
@@ -11,13 +11,13 @@ namespace VidlyUA.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
-        //[Required]
-        //public Genre Genre { get; set; }
-        //public int GenereId { get; set; }
+        [Required]
+        public Genre Genre { get; set; }
+        public int GenreId { get; set; }
 
-        //public DateTime ReleasedDate { get; set; }
-        //public DateTime DateAdded { get; set; }
-        //public int NumberInStock { get; set; }
+        public DateTime ReleasedDate { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int NumberInStock { get; set; }
 
     }
 
