e98c0236368497feb1cf670d02e41688b211fdf8
Stephen Parsons <zup_dudes@yahoo.com>
2018-08-02 08:15:30 -0400

    FIX - DB CONTEXT ISSUE -- The model backing the 'ApplicationDbContext' context has changed since the database was created

--- VidlyUA/Global.asax.cs
+++ VidlyUA/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +17,13 @@ namespace VidlyUA
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //
+            // The following line added because of this failing messaage when trying to run ToList() on my Enums
+            //  The model backing the 'ApplicationDbContext' context has changed since the database was created
+            //  !!! WATCH FOR DOWN STREAM PROBLEMS. NOT SURE WHAT IS GETTING FIXED BY ADDING THE CODE !!!//
+            //
+            Database.SetInitializer<Models.ApplicationDbContext>(null);
         }
     }
 }
