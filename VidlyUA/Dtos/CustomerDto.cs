e38a3cdd5cbfffc26b2c0496ee8d27b0420d91fd
Stephen Parsons <zup_dudes@yahoo.com>
2018-08-03 08:41:54 -0400

    Add initial WebApi -- Use Mapper

---
+++ VidlyUA/Dtos/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VidlyUA.Models;
+
+namespace VidlyUA.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required( ErrorMessage = "Please enter a valid Name" )]
+        [StringLength( 255 )]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNew { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? BirthDateTime { get; set; }
+    }
+}
--- VidlyUA/Global.asax.cs
+++ VidlyUA/Global.asax.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using VidlyUA.App_Start;
 
 namespace VidlyUA
 {
@@ -13,6 +16,8 @@ namespace VidlyUA
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure( WebApiConfig.Register );
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
